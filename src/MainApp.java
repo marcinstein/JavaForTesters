f09ccd6018bcb6069b30d3cca32ffc5c9002a2bf
Marcin Stein <marcin.stein@gmail.com>
2020-11-15 19:05:54 +0100

    Changed MainAppl to MainApp

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,4 +1,4 @@
-public class MainAppl {
+public class MainApp {
     public static void main(String[] args) {
         System.out.println("Main branch. Each further branch contains different lessons");
     }
