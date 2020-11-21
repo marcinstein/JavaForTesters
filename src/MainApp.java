06c1ab804d414e124bf6d61d246f4cf0a55fcb65
Marcin Stein <marcin.stein@gmail.com>
2020-11-21 20:07:44 +0100

    Lesson1

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,28 @@
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Main branch. Each further branch contains different lessons");
+
+        // Application to check if sum of a nd b is odd or even, sum of c and d is odd or even.
+        // Application check if all sums are even or odd
+        int a = 324;
+        int b = 2342;
+        int c = 2652;
+        int d = 9876;
+
+        if ((a + b) % 2 == 0) {
+            System.out.println("Sum of a and b is even");
+        } else {
+            System.out.println("Sum of a and b is odd");
+        }
+        if ((c + d) % 2 == 0) {
+            System.out.println("Sum of c and d is even");
+        } else {
+            System.out.println("Sum of c and d is odd");
+        }
+
+        if ((a + b) % 2 == 0 && (c + d) % 2 == 0) {
+            System.out.println("Sum of a and b is even and sum of c and d is even");
+        } else {
+            System.out.println("Sum of a and b or sum of c and d is odd");
+        }
     }
 }
